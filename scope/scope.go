96727c1264dcf1f6d84a81c21f8b2cb3c0915dfe
kedebug <sunweiqq@gmail.com>
2014-06-15 00:34:34 +0800

    add Scope interface

--- scope/scope.go
+++ scope/scope.go
@@ -1,4 +1,26 @@
 package scope
 
+import (
+  "github.com/kedebug/LispEx/value"
+)
+
 type Scope struct {
+  parent *Scope
+  env    map[string]interface{}
+}
+
+func NewScope(parent *Scope) *Scope {
+  return &Scope{
+    parent: parent,
+    env:    make(map[string]interface{}),
+  }
+}
+
+func NewRootScope() *Scope {
+  root := NewScope(nil)
+  return root
+}
+
+func (self *Scope) PutValue(name string, value value.Value) {
+  self.env[name] = value
 }
