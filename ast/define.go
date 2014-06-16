8814524cff170f96203ec5a7304a6d4f67462018
kedebug <sunweiqq@gmail.com>
2014-06-17 00:37:46 +0800

    add AST node: Name, Define

--- ast/define.go
+++ ast/define.go
@@ -1,1 +1,19 @@
 package ast
+
+import (
+  "github.com/kedebug/LispEx/scope"
+  "github.com/kedebug/LispEx/value"
+)
+
+type Define struct {
+  Pattern Node
+  Value   Node
+}
+
+func NewDefine(pattern, val Node) *Define {
+  return &Define{Pattern: pattern, Value: val}
+}
+
+func (self *Define) Eval() value.Value {
+
+}
---
+++ ast/name.go
@@ -0,0 +1,18 @@
+package ast
+
+import (
+  "github.com/kedebug/LispEx/scope"
+  "github.com/kedebug/LispEx/value"
+)
+
+type Name struct {
+  Identifier string
+}
+
+func NewName(identifier string) *Name {
+  return &Name{Identifier: identifier}
+}
+
+func (self *Name) Eval(env *scope.Scope) value.Value {
+  return env.Lookup(self.Identifier)
+}
