132da06f2b81e884d2f136b9df5d4fc8c173865c
kedebug <sunweiqq@gmail.com>
2014-06-18 00:25:45 +0800

    add Function and Closure

---
+++ ast/function.go
@@ -0,0 +1,25 @@
+package ast
+
+import (
+  "fmt"
+  "github.com/kedebug/LispEx/scope"
+  "github.com/kedebug/LispEx/value"
+  "github.com/kedebug/LispEx/value/closure"
+)
+
+type Function struct {
+  Params []Name
+  Body   Node
+}
+
+func NewFunction(params []Name, body Node) *Function {
+  return &Function{Params: params, Body: body}
+}
+
+func (self *Function) Eval(env *scope.Scope) value.Value {
+  return &closure.NewClosure(env, self)
+}
+
+func (self *Function) String() string {
+  return fmt.Sprint(self.Body)
+}
