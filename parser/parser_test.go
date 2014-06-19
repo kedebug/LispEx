116382ef4392d0e0d372a09ba32021c2b619df86
kedebug <sunweiqq@gmail.com>
2014-06-19 22:03:13 +0800

    rename

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -8,7 +8,7 @@ import (
 func TestParser(t *testing.T) {
   var exprs string = `
   (f 1 2) (g 1 2)
-  (((f 1 2) 3 4) 5 6
+  (((f 1 2) 3 4) 5 6)
   (f (1 2 (3 4 (5 6))))
   `
   block := ParseFromString("Parser", exprs)
