3ed78620b7492209eccc02cc69f60c748209e486
kedebug <sunweiqq@gmail.com>
2014-06-20 19:48:29 +0800

    add Parser prototype

--- parser/parser.go
+++ parser/parser.go
@@ -11,8 +11,10 @@ func ParseFromString(name, program string) ast.Node {
 }
 
 func Parse(l *lexer.Lexer) ast.Node {
-  elements := parser(l, make([]ast.Node, 0), ' ')
-  return ast.NewBlock(elements)
+  var elements []ast.Node
+  elements = append(elements, ast.NewName("seq"))
+  elements = append(elements, parser(l, make([]ast.Node, 0), ' ')...)
+  return ast.NewTuple(elements)
 }
 
 func parser(l *lexer.Lexer, elements []ast.Node, seek rune) []ast.Node {
@@ -41,9 +43,48 @@ func parser(l *lexer.Lexer, elements []ast.Node, seek rune) []ast.Node {
       panic(fmt.Errorf("unexpected token type: %v", token.Type))
     }
   }
-
   if seek != ' ' {
     panic(fmt.Errorf("unclosed delimeter, expected: '%c'", seek))
   }
   return elements
+}
+
+func ParseNode(node ast.Node) ast.Node {
+  tuple, ok := node.(*ast.Tuple)
+  if !ok {
+    return node
+  }
+  elements := tuple.Elements
+  if len(elements) == 0 {
+    panic(fmt.Errorf("syntax error, empty list"))
+  }
+  switch elements[0].(type) {
+  case *ast.Name:
+    name := elements[0].(*ast.Name)
+    switch name.Identifier {
+    case "seq":
+      return ParseBlock(tuple)
+    case "define":
+      return ParseDefine(tuple)
+    default:
+      return ParseCall(tuple)
+    }
+  case *ast.Tuple:
+    // ((foo 1) 2)
+    return ParseCall(tuple)
+  default:
+    panic(fmt.Errorf("unexpected node: %s", elements[0]))
+  }
+}
+
+func ParseBlock(tuple *ast.Tuple) ast.Node {
+
+}
+
+func ParseDefine(tuple *ast.Tuple) ast.Node {
+
+}
+
+func ParseCall(tuple *ast.Tuple) ast.Node {
+
 }
