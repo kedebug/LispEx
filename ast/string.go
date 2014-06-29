79796ca458a428011ef619413ab00dd319a8ac7b
kedebug <sunweiqq@gmail.com>
2014-06-29 15:54:08 +0800

    Add literal string token

---
+++ ast/string.go
@@ -0,0 +1,23 @@
+package ast
+
+import (
+  "fmt"
+  "github.com/kedebug/LispEx/scope"
+  "github.com/kedebug/LispEx/value"
+)
+
+type String struct {
+  Value string
+}
+
+func NewString(val string) *String {
+  return &String{Value: val[1 : len(val)-1]}
+}
+
+func (self *String) Eval(env *scope.Scope) value.Value {
+  return value.NewStringValue(self.Value)
+}
+
+func (self *String) String() string {
+  return fmt.Sprintf("\"%s\"", self.Value)
+}
