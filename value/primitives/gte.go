6c67c0825e9762e80c0dd3dbca65f7667693a90e
kedebug <sunweiqq@gmail.com>
2014-06-29 21:12:51 +0800

    Add binary operator primitives

---
+++ value/primitives/gte.go
@@ -0,0 +1,34 @@
+package primitives
+
+import (
+  "fmt"
+  "github.com/kedebug/LispEx/value"
+)
+
+type GtE struct {
+  value.Primitive
+}
+
+func NewGtE() *GtE {
+  return &GtE{value.Primitive{">="}}
+}
+
+func (self *GtE) Apply(args []value.Value) value.Value {
+  if len(args) != 2 {
+    panic(fmt.Sprint("argument mismatch for `>=', expected 2, given: ", len(args)))
+  }
+  if v1, ok := args[0].(*value.IntValue); ok {
+    if v2, ok := args[1].(*value.IntValue); ok {
+      return value.NewBoolValue(v1.Value >= v2.Value)
+    } else if v2, ok := args[1].(*value.FloatValue); ok {
+      return value.NewBoolValue(float64(v1.Value) >= v2.Value)
+    }
+  } else if v1, ok := args[0].(*value.FloatValue); ok {
+    if v2, ok := args[1].(*value.IntValue); ok {
+      return value.NewBoolValue(v1.Value >= float64(v2.Value))
+    } else if v2, ok := args[1].(*value.FloatValue); ok {
+      return value.NewBoolValue(v1.Value >= v2.Value)
+    }
+  }
+  panic(fmt.Sprint("incorrect argument type for `>=', expected number?"))
+}
